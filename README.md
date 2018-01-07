a7f0eb2afd7011ef0df7ce9668150bbce48ccffd
Marco Link <mfl-github@bitblox.de>
2018-01-07 21:00:31 +0100

    First working version.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Minetest-Mod: name\_wielded\_item
+
+## What is this?
+
+This is a small mod for minetest, which shows the description (or name) of the wielded item on the HUD.
+
+## How to install?
+
+Locate the mods-folder of your minetest(-server)-installation.
+
+Click the *clone or download*-button in GitHub-Repository and download the ZIP-file to the mods-folder by clicking *Download ZIP* or if you have git installed call the following in your mods-folder:
+	
+	git clone git@github.com:mflink3012/name_wielded_item.git
+
+Now you can select this mod in you minetest-client for singleplayer (e.g. for testing). If you want to use it on your minetest-server you have to edit the world's corresponding ``world.mt`` (inside the specific world-folder) and add
+
+	load_mod_name_wielded_item = true
+
+to its end.
+
+## Which dependencies?
+
+See shipped ``depends.txt``.
+
+## How to use?
+
+1. Just look on the right-bottom-corner with an item in your hand to see its description/name.
+
+## Known issues?
+
+See the ``issues``-tab in GitHub-Repository.
+
+## Tested with?
+
+minetest 0.4.15
+
+## What license?
+
+GPL 3.0 (See the ``LICENSE``-file shipped or <https://www.gnu.org/licenses/gpl-3.0.txt> for details.)
---
+++ init.lua
@@ -0,0 +1,80 @@
+local modName = minetest.get_current_modname();
+
+local name_wielded_item = {
+  update_time = 0.25,
+  dtime = 0,
+  hud_to_player = {}
+}
+
+function name_wielded_item.add(player)
+    local index_text = player:hud_add({
+        hud_elem_type = "text",
+        position  = {x = 1, y = 1},
+        offset    = {x = -90, y = -30},
+        text      = "",
+        alignment = 0,
+        scale     = { x = 100, y = 30},
+        number    = 0x00FF00,
+    })
+    local player_name = player:get_player_name()
+
+    name_wielded_item.hud_to_player[player_name] = {
+      index_text = index_text
+    }
+end
+
+function name_wielded_item.remove(player)
+    local player_name = player:get_player_name()
+
+    if name_wielded_item.hud_to_player[player_name] ~= nil then
+      player:hud_remove(name_wielded_item.hud_to_player[player_name].index_text)
+    end
+end
+
+function name_wielded_item.update(player)
+  local wielded_item = player:get_wielded_item()
+  local player_name = player:get_player_name()
+  
+  if wielded_item ~= nil then
+    local wielded_item_name = wielded_item:get_name()
+
+    if wielded_item:get_definition().description ~= nil then
+      wielded_item_name = wielded_item:get_definition().description 
+    end
+
+    if name_wielded_item.hud_to_player[player_name] ~= nil then
+      player:hud_change(name_wielded_item.hud_to_player[player_name].index_text, "text", wielded_item_name)
+    else
+      name_wielded_item.add(player)
+    end
+  else
+    player:hud_change(name_wielded_item.hud_to_player[player_name].index_text, "text", "")
+  end
+end
+
+function name_wielded_item.on_globalstep(dtime)
+  name_wielded_item.dtime = name_wielded_item.dtime + dtime
+  if name_wielded_item.dtime > name_wielded_item.update_time then
+    for _,player in ipairs(minetest.get_connected_players()) do
+      name_wielded_item.update(player)
+    end
+
+    name_wielded_item.dtime = 0
+  end
+end
+
+function name_wielded_item.on_joinplayer(player)
+  name_wielded_item.add(player)
+end
+
+function name_wielded_item.on_leaveplayer(player)
+  name_wielded_item.remove(player)
+end
+
+minetest.register_on_joinplayer(name_wielded_item.on_joinplayer)
+minetest.register_on_leaveplayer(name_wielded_item.on_leaveplayer)
+minetest.register_globalstep(name_wielded_item.on_globalstep)
+
+if minetest.setting_get("log_mods") then
+  minetest.log("action", "[mod/" .. modName .. "] loaded.")
+end
